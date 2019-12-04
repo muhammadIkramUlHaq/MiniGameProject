ea7ca9be7ee936cccc3809c161fbaa1658349550
m.haq <m.haq@cartelsolutions.com>
2019-12-04 03:19:21 +0100

    Added run-time argument support for port number.

--- src/main/java/com/king/game/MiniGameServer.java
+++ src/main/java/com/king/game/MiniGameServer.java
@@ -14,13 +14,17 @@ public class MiniGameServer {
     private final static Logger logger = Logger.getLogger(MiniGameServer.class.getName());
 
     public static void main(String[] args) {
-        start();
+        if (args.length > 0) {
+            start(Integer.parseInt(args[0]));
+        } else {
+            start(PORT_NUMBER);
+        }
     }
 
-    public static void start() {
+    public static void start(int portNumber) {
         try {
-            HttpServer httpServer = HttpServer.create(new InetSocketAddress(PORT_NUMBER), 0);
-            logger.info("Server is started at port: " + PORT_NUMBER);
+            HttpServer httpServer = HttpServer.create(new InetSocketAddress(portNumber), 0);
+            logger.info("Server is started at port: " + portNumber);
             HttpContext httpContext = httpServer.createContext(ROOT, new RootHandler());
             httpServer.setExecutor(null);
             httpServer.start();
